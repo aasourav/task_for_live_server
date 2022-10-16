9949eb11f43a9c930510097c10bbea6a7ef6e703
Ahsan Amin <ahsan.amin.eu181400095@gmail.com>
2022-10-16 23:01:25 +0600

    problem - 1

--- back/index.js
+++ back/index.js
@@ -17,21 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 
-// app.use(cors({credentials:true, origin:"https://taskfontend.vercel.app",}))
-
-app.use(cors({
-    "headers": [
-      {
-        "source": "https://taskfontend.vercel.app/",
-        "headers": [
-          { "key": "Access-Control-Allow-Credentials", "value": "true" },
-          { "key": "Access-Control-Allow-Origin", "value": "*" },
-          { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-        ]
-      }
-    ]
-  }))
+app.use(cors({credentials:true, origin:"https://taskfontend.vercel.app",}))
 
 mongoose.connect(`${process.env.Mongo}`).then(()=>{
     console.log("Mongodb Connected")
