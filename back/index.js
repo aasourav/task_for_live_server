e50d5da7066168d48857a1b558e13dce1bd9f97c
Ahsan Amin <ahsan.amin.eu181400095@gmail.com>
2022-10-20 14:55:22 +0600

    header updated on backend

--- back/index.js
+++ back/index.js
@@ -18,6 +18,18 @@ app.use(cookieParser())
 
 
 app.use(cors({credentials:true, origin:"https://taskfontend.vercel.app",}))
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    );
+    res.header(
+       "Access-Control-Allow-Headers",
+       "Origin,X-Requested-with,Content-Type,Accept,Authorization" 
+    );
+    next();
+})
 
 mongoose.connect(`${process.env.Mongo}`).then(()=>{
     console.log("Mongodb Connected")
